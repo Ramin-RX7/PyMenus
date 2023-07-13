c2a701c1ba05b08765a2239d7732e7f297fa5cf5
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-14 02:55:17 +0330

    add structural menu example

---
+++ examples/4_Structural.py
@@ -0,0 +1,29 @@
+from pymenus import Menu,Option,BACK_BUTTON,SEPARATOR
+
+
+
+
+data_menu = Menu(title="Data and Storage")
+
+
+def change_password_function():
+    print("password can not be changed")
+def change_name_function(new_name):
+    print(f"your new name is: {new_name}")
+
+privacy_menu = Menu.parse_structure(title="Privacy", structure=[
+    "List of Available options:",
+    Option(title="Change name", function=change_name_function, kwargs={"new_name":"MY_NEW_NAME"}),
+    Option(title="Change password", function=change_password_function)
+])
+
+
+main_menu = Menu.parse_structure(title="MainMenu", structure=[
+    "List of Menus:",
+    privacy_menu,
+    data_menu,
+    SEPARATOR,
+    BACK_BUTTON
+])
+
+main_menu.execute()
